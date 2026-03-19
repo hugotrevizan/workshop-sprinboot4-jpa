d213367dae619bfe9cb42bfc91d739bfb1c97bc7
Hugo Trevizan <hugo.trevizan@gmail.com>
2026-03-18 22:06:02 -0300

    Category

---
+++ src/main/java/com/educandoweb/course/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.educandoweb.course.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
